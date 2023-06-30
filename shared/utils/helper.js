806496befc593e9fda28bda05755b5e6db1ffdde
“Iliyasooo7” <iliyas.shaikh19@vit.edu>
2023-06-30 15:23:45 +0530

    Yet to be checked

--- shared/utils/helper.js
+++ shared/utils/helper.js
@@ -79,3 +79,21 @@ export const validate = (schema, options = {}) => {
     next();
   };
 };
+
+/*
+// Check if customer is logged In
+export const isLoggedIn = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1]
+    const decoded = jwt.verify(token, process.env.JWT_KEY)
+    req.userData = decoded
+    console.log('here in islogged in');
+    next();
+  } catch (err) {
+    next({
+      status: 401,
+      message: 'Invalid authorization'
+    })
+  }
+}
+*/

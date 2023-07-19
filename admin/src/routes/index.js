cf8161914c27bb7e919475fa029ee84ac5bfbe86
“Iliyasooo7” <iliyas.shaikh19@vit.edu>
2023-07-19 13:01:19 +0530

    Category [get ADD Update Delete]

--- admin/src/routes/index.js
+++ admin/src/routes/index.js
@@ -1,11 +1,13 @@
 import { sendResponse } from "../../../shared/utils/helper.js";
 import adminRouter from "./admin.js";
 import adminServiceRouter from "./services.js"
+import adminCategory from "./category.js"
 import Router from "express";
 
 const router = Router();
 
 router.use("/", adminRouter);
+router.use("/category", adminCategory);
 router.use("/service", adminServiceRouter);
 
 
--- shared/models/category.js
+++ shared/models/category.js
@@ -4,6 +4,7 @@ const schema = mongoose.Schema(
     name: { type: String, required: true },
     type: { type: String, required: true },
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'admin' }, 
+    isActive: {  type: Boolean, default:true, required: true }
   },
   {
     timestamp: true,

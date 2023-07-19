32d2610f807257b86d85d9ef9df5e85a6e0d1453
“Iliyasooo7” <iliyas.shaikh19@vit.edu>
2023-07-19 22:40:41 +0530

    firstCommit

---
+++ customer/src/routes/index.js
@@ -0,0 +1,14 @@
+import { sendResponse } from "../../../shared/utils/helper.js";
+import userRouter from "./user.js";
+import Router from "express";
+
+const router = Router();
+
+router.use("/", userRouter);
+
+
+router.use((req, res) => {
+  sendResponse(res, 404, "Route Not Found", null, { path: req.path });
+});
+
+export default router;
---
+++ vendor/src/index.js
@@ -0,0 +1,51 @@
+import 'express-async-errors'
+import boot from "./utils/boot.js";
+import express from "express";
+import { sendResponse } from "../../shared/utils/helper.js";
+import routes from "./routes/index.js";
+const app = express();
+
+app.set("reverse proxy", 1);
+
+app.options("/*", (_, res) => {
+  res.sendStatus(200);
+});
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(routes);
+
+app.get("/", (req, res) => {
+  sendResponse(res, 200, "Server is working");
+});
+
+/*
+app.use((err, req, res, next) => {
+  //   console.error(err);
+  if (err) {
+    sendResponse(res, 500, err.message);
+  }
+});
+
+*/
+app.use((err, req, res, next) => {
+  console.error(err);
+  sendResponse(res, 500, "internal server error",null,err);
+});
+ 
+// app.use((req, res) => {
+//   sendResponse(res, 404, "Route Not Found", { path: req.path });
+// });
+
+boot().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Service is listening on port ${process.env.PORT}`);
+  });
+});
+
+export default app;

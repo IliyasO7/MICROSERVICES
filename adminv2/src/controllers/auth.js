fb26735c4ee247c4867cc1cd46e8bf29b5ce01ce
“Iliyasooo7” <iliyas.shaikh19@vit.edu>
2023-07-27 13:42:19 +0530

    admin seperated

--- adminv2/src/controllers/auth.js
+++ adminv2/src/controllers/auth.js
@@ -4,60 +4,126 @@ import { generateTokens } from "../../../shared/utils/token.js";
 import { sendResponse } from "../../../shared/utils/helper.js";
 
 export const createSuperAdmin = async (req, res) => {
+  const username = req.body.username;
+  const role = req.body.role;
+  const fname = req.body.fname;
+  const lname = req.body.lname;
+  const email = req.body.email;
+  const password = req.body.password;
 
-    const username = req.body.username;
-    const role = req.body.role;
-    const fname = req.body.fname;
-    const lname = req.body.lname;
-    const email = req.body.email;
-    const password = req.body.password;
+  const encryptedPassword = await bcrypt.hash(password, 10);
+  const adminExists = await Admin.findOne({ email: email });
 
-            let encryptedPassword = await bcrypt.hash(password, 10)
-            let adminExists = await Admin.findOne({ email: email });
+  if (adminExists) {
+    return sendResponse(res, 400, "Admin Already Exists");
+  }
 
-            if(adminExists){
-                return sendResponse(res, 400, "Admin Already Exists");
-            }
+  const totalAdmins = await Admin.find({ role: "HJ-SUPER-ADMIN" });
+  const count = totalAdmins.length;
+  const currentAdminNo = count + 1;
+  // let currentAdminNo =  1;
+  const sku = `HJ-SUPER-ADMIN-${currentAdminNo}`;
 
-            const totalAdmins = await Admin.find({role:'HJ-SUPER-ADMIN'});
-            let count = totalAdmins.length;
-            console.log('total Admins',count);
-            let currentAdminNo = count + 1;
-           // let currentAdminNo =  1;
-            const sku = `HJ-SUPER-ADMIN-${currentAdminNo}`;
-           // const sku = `HJ-RENTAL-ADMIN-${currentAdminNo}`
-      
-            const saveUser = await Admin.create({ 
-                adminId: sku,
-                username: username,
-                role:`HJ-SUPER-ADMIN`,
-                fname:fname,
-                lname:lname,
-                email:email,
-                password:encryptedPassword,
-
-            })
-        if(saveUser) sendResponse(res, 200, "Super Admin Created Successfully", {saveUser});
-        
+  const data = await Admin.create({
+    adminId: sku,
+    username: username,
+    role: `HJ-SUPER-ADMIN`,
+    fname: fname,
+    lname: lname,
+    email: email,
+    password: encryptedPassword,
+  });
+  if (saveUser)
+    sendResponse(res, 200, "Super Admin Created Successfully", data);
 };
 
 export const loginAdmin = async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-    const username = req.body.username;
-    const password = req.body.password;
+  const adminData = await Admin.findOne({ username: username });
 
-    let adminData = await Admin.findOne({username:username})
-
-    if(!adminData){
-         return sendResponse(res, 400, "Admin Does Not Exists");
-      }
-     else{
-        let verifyPassword = await bcrypt.compare(password, adminData.password)
-        if(verifyPassword){
-            const tokens = generateTokens({ userId: adminData._id });
-            return sendResponse(res, 200, "Admin Login Successfully", {tokens,adminData});
-        } 
-     }     
+  if (!adminData) {
+    return sendResponse(res, 400, "Admin Does Not Exists");
+  } else {
+    const verifyPassword = await bcrypt.compare(password, adminData.password);
+    if (verifyPassword) {
+      const tokens = generateTokens({ userId: adminData._id });
+      return sendResponse(res, 200, "Admin Login Successfully", {
+        tokens,
+        adminData,
+      });
+    }
+  }
 };
 
+export const createOdsAdmin = async (req, res) => {
+  console.log("create ODS ADMIN");
 
+  const superAdmin = req.user;
+  const username = req.body.username;
+  const role = req.body.role;
+  const fname = req.body.fname;
+  const lname = req.body.lname;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const encryptedPassword = await bcrypt.hash(password, 10);
+  const adminExists = await Admin.findOne({ email: email });
+
+  if (adminExists) {
+    return sendResponse(res, 400, "Admin Already Exists");
+  }
+
+  const totalAdmins = await Admin.find({ role: "HJ-ODS-ADMIN" });
+  const count = totalAdmins.length;
+  const currentAdminNo = count + 1;
+  // let currentAdminNo =  1;
+  const sku = `HJ-ODS-ADMIN-${currentAdminNo}`;
+
+  const data = await Admin.create({
+    adminId: sku,
+    username: username,
+    role: role,
+    fname: fname,
+    lname: lname,
+    email: email,
+    password: encryptedPassword,
+    createdBy: superAdmin._id,
+  });
+  return sendResponse(res, 200, "Ods Admin Created Successfully", data);
+};
+
+export const createRentalAdmin = async (req, res) => {
+  const superAdmin = req.user;
+  const username = req.body.username;
+  const role = req.body.role;
+  const fname = req.body.fname;
+  const lname = req.body.lname;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const encryptedPassword = await bcrypt.hash(password, 10);
+  const adminExists = await Admin.findOne({ email: email });
+
+  if (adminExists) {
+    return sendResponse(res, 400, "Admin Already Exists");
+  }
+  const totalAdmins = await Admin.find({ role: "HJ-RENTAL-ADMIN" });
+  const count = totalAdmins.length;
+
+  const currentAdminNo = count + 1;
+  const sku = `HJ-RENTAL-ADMIN-${currentAdminNo}`;
+
+  const data = await Admin.create({
+    adminId: sku,
+    username: username,
+    role: `HJ-RENTAL-ADMIN`,
+    fname: fname,
+    lname: lname,
+    email: email,
+    password: encryptedPassword,
+    createdBy: superAdmin._id,
+  });
+  return sendResponse(res, 200, "Rental Admin Created Successfully", data);
+};
--- adminv2/src/routes/auth.js
+++ adminv2/src/routes/auth.js
@@ -1,17 +1,30 @@
-import * as controller from "../controllers/auth.js"
+import * as controller from "../controllers/auth.js";
 import Router from "express";
 import validation from "../validation/admin.js";
-import { validate,checkAuthAdmin } from "../../../shared/utils/helper.js";
+import { validate } from "../../../shared/utils/helper.js";
+import { checkAuth } from "../middleware/checkAuth.js";
 
 const router = Router();
 
-router.post('/login',
-validate(validation.adminlogin),
-controller.loginAdmin);
+router.post("/login", validate(validation.adminlogin), controller.loginAdmin);
 
 //create Super admin
 router
-  .route('/create-super-admin') 
-  .post(validate(validation.adminCreate), controller.createSuperAdmin)
+  .route("/create-super-admin")
+  .post(validate(validation.adminCreate), controller.createSuperAdmin);
+
+router.post(
+  "/create-ods-admin",
+  checkAuth(),
+  validate(validation.adminCreate),
+  controller.createOdsAdmin
+);
+
+router.post(
+  "/create-rental-admin",
+  checkAuth(),
+  validate(validation.adminCreate),
+  controller.createRentalAdmin
+);
 
 export default router;

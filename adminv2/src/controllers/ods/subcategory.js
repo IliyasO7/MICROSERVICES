aa4f15112f9af7b7647df9123d8c892c84dd95db
Yuva <yuvapoojary@gmail.com>
2023-08-01 02:28:32 +0530

    vendor model

--- adminv2/src/controllers/ods/subcategory.js
+++ adminv2/src/controllers/ods/subcategory.js
@@ -1,6 +1,5 @@
-import Category from "../../../../shared/models/category.js";
-import ServiceSubcategory from "../../../models/ods/subcategory.js";
-import { sendResponse } from "../../../../shared/utils/helper.js";
+import ServiceSubcategory from '../../../models/ods/subcategory.js';
+import { sendResponse } from '../../../../shared/utils/helper.js';
 
 export const createSubCategory = async (req, res) => {
   const data = new ServiceSubcategory({
@@ -9,48 +8,52 @@ export const createSubCategory = async (req, res) => {
 
   await data.save();
 
-  sendResponse(res, 200, "success", data);
+  sendResponse(res, 200, 'success', data);
 };
 
 export const getSubCategory = async (req, res) => {
   const filter = {};
 
   if (req.query.name) {
-    filter["name"] = new RegExp(req.query.name, "i");
+    filter['name'] = new RegExp(req.query.name, 'i');
+  }
+
+  if (req.query.category) {
+    (filter['category'] = req), query.category;
   }
 
   if (req.query.isEnabled) {
-    filter["isEnabled"] = req.query.isEnabled;
+    filter['isEnabled'] = req.query.isEnabled;
   }
 
   const data = await ServiceSubcategory.find(filter).lean();
 
-  sendResponse(res, 200, "success", data);
+  sendResponse(res, 200, 'success', data);
 };
 
 export const getSubCategoryById = async (req, res) => {
   const data = await ServiceSubcategory.findById(req.params.id).lean();
-  if (!data) return sendResponse(res, 404, "category not found");
+  if (!data) return sendResponse(res, 404, 'category not found');
 
-  sendResponse(res, 200, "success", data);
+  sendResponse(res, 200, 'success', data);
 };
 
 export const updateSubCategory = async (req, res) => {
   const data = await ServiceSubcategory.findById(req.params.id);
-  if (!data) return sendResponse(res, 404, "category not found");
+  if (!data) return sendResponse(res, 404, 'category not found');
 
   Object.assign(data, req.body);
 
   await data.save();
 
-  sendResponse(res, 200, "success", data);
+  sendResponse(res, 200, 'success', data);
 };
 
 export const deleteSubCategory = async (req, res) => {
   const data = await ServiceSubcategory.findById(req.params.id);
-  if (!data) return sendResponse(res, 404, "category not found");
+  if (!data) return sendResponse(res, 404, 'category not found');
 
   await data.deleteOne();
 
-  sendResponse(res, 200, "success");
+  sendResponse(res, 200, 'success');
 };

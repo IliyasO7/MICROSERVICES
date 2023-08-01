033b29cab0a18e6ff72b957375c64f43f77f6deb
“Iliyasooo7” <iliyas.shaikh19@vit.edu>
2023-08-01 17:19:14 +0530

    udpated Prettier

--- adminv2/src/routes/ods/service.js
+++ adminv2/src/routes/ods/service.js
@@ -1,17 +1,17 @@
-import { Router } from 'express';
-import * as controller from '../../controllers/ods/service.js';
-import * as validation from '../../validation/ods/service.js';
-import { validate } from '../../../../shared/utils/helper.js';
+import { Router } from "express";
+import * as controller from "../../controllers/ods/service.js";
+import * as validation from "../../validation/ods/service.js";
+import { validate } from "../../../../shared/utils/helper.js";
 
 const router = Router();
 
 router
-  .route('/')
+  .route("/")
   .get(controller.getServices)
   .post(validate(validation.createService), controller.createService);
 
 router
-  .route('/:id')
+  .route("/:id")
   .get(controller.getServiceById)
   .patch(validate(validation.updateService), controller.updateService)
   .delete(controller.deleteService);

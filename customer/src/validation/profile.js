92bcd483fc6a7603e18afd1f8ca9c1cebc60874f
Yuva <yuvapoojary@gmail.com>
2023-08-10 22:51:58 +0530

    fix: add new fields in address

--- customer/src/validation/profile.js
+++ customer/src/validation/profile.js
@@ -13,8 +13,11 @@ export const updateProfile = joi.object({
 });
 
 export const createAddress = joi.object({
-  mobile: joi.string(),
-  address: joi.string().required(),
+  fname: joi.string().required(),
+  lname: joi.string().required(),
+  mobile: joi.string().length(10),
+  line1: joi.string().required(),
+  line2: joi.string().allow(''),
   landmark: joi.string().allow(''),
   city: joi.string().lowercase().required(),
   state: joi.string().lowercase().required(),
@@ -22,8 +25,11 @@ export const createAddress = joi.object({
 });
 
 export const updateAddress = joi.object({
-  mobile: joi.string(),
-  address: joi.string(),
+  fname: joi.string(),
+  lname: joi.string(),
+  mobile: joi.string().length(10),
+  line1: joi.string(),
+  line2: joi.string().allow(''),
   landmark: joi.string().allow(''),
   city: joi.string().lowercase(),
   state: joi.string().lowercase(),
--- shared/models/address.js
+++ shared/models/address.js
@@ -1,15 +1,18 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const schema = new mongoose.Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
+    fname: { type: String },
+    lname: { type: String },
     mobile: { type: String },
-    address: { type: String, default: null },
-    landmark: { type: String, default: "" },
+    line1: { type: String },
+    line2: { type: String },
+    landmark: { type: String, default: '' },
     city: { type: String, default: null },
     state: { type: String, default: null },
     pincode: { type: String, default: null },
-    country: { type: String, default: "in" },
+    country: { type: String, default: 'in' },
     isDefault: { type: Boolean, default: false },
   },
   {
@@ -17,6 +20,6 @@ const schema = new mongoose.Schema(
   }
 );
 
-const Address = mongoose.model("address", schema);
+const Address = mongoose.model('address', schema);
 
 export default Address;

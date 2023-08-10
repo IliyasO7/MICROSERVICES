5ce9f6b4497d88ae251b2a24e60838e2f20b49ca
“Iliyasooo7” <iliyas.shaikh19@vit.edu>
2023-08-10 11:23:53 +0530

    checkAuth removed from services and added to cart

--- customer/src/routes/ods/index.js
+++ customer/src/routes/ods/index.js
@@ -1,12 +1,12 @@
-import { Router } from 'express';
-import serviceRoutes from './service.js';
-import cartRoutes from './cart.js';
-import orderRoutes from './order.js';
-
+import { Router } from "express";
+import serviceRoutes from "./service.js";
+import cartRoutes from "./cart.js";
+import orderRoutes from "./order.js";
+import { checkAuth } from "../../middleware/checkAuth.js";
 const router = Router();
 
-router.use('/cart', cartRoutes);
-router.use('/orders', orderRoutes);
-router.use('/services', serviceRoutes);
+router.use("/cart", checkAuth(), cartRoutes);
+router.use("/orders", checkAuth(), orderRoutes);
+router.use("/services", serviceRoutes);
 
 export default router;

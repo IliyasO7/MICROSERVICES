6f1709924ea6d448c1e2e57fc5e41d483ce1414e
“Iliyasooo7” <iliyas.shaikh19@vit.edu>
2023-08-14 16:12:53 +0530

    payU create and verify Hash updated

--- customer/src/controllers/payU.js
+++ customer/src/controllers/payU.js
@@ -1,56 +1,59 @@
-import jsSHA from 'jssha';
+import jsSHA from "jssha";
+import { sendResponse } from "../../../../shared/utils/helper.js";
 
 export const payUMoneyPayment = async (req, res) => {
   var pd = req.body;
   var hashString =
     process.env.PAYUMERCHANTKEY + // Merchant Key
-    '|' +
+    "|" +
     pd.txnid +
-    '|' +
+    "|" +
     pd.amount +
-    '|' +
+    "|" +
     pd.productinfo +
-    '|' +
+    "|" +
     pd.firstname +
-    '|' +
+    "|" +
     pd.email +
-    '|' +
-    '||||||||||' +
+    "|" +
+    "||||||||||" +
     process.env.PAYUSALT; // Your salt value
-  var sha = new jsSHA('SHA-512', 'TEXT');
+  var sha = new jsSHA("SHA-512", "TEXT");
   sha.update(hashString);
-  var hash = sha.getHash('HEX');
-  sendResponse(res, 200, 'success', hash);
+  var hash = sha.getHash("HEX");
+  sendResponse(res, 200, "success", hash);
 };
 
 export const payUResponse = async (req, res) => {
   var pd = req.body;
+  console.log("Req BODY", req.body);
 
   //Generate new Hash
   var hashString =
-    process.env.PAYUSALT +
-    '|' +
-    pd.status +
-    '||||||||||' +
-    '|' +
-    pd.email +
-    '|' +
-    pd.firstname +
-    '|' +
-    pd.productinfo +
-    '|' +
-    pd.amount +
-    '|' +
+    process.env.PAYUMERCHANTKEY + // Merchant Key
+    "|" +
     pd.txnid +
-    '|' +
-    process.env.PAYUMERCHANTKEY;
-  var sha = new jsSHA('SHA-512', 'TEXT');
+    "|" +
+    pd.amount +
+    "|" +
+    pd.productinfo +
+    "|" +
+    pd.firstname +
+    "|" +
+    pd.email +
+    "|" +
+    "||||||||||" +
+    process.env.PAYUSALT; // Your salt value
+  var sha = new jsSHA("SHA-512", "TEXT");
   sha.update(hashString);
-  var hash = sha.getHash('HEX');
+  var hash = sha.getHash("HEX");
   // Verify the new hash with the hash value in response
+  console.log("PD HASH", pd);
+  console.log("HASH", hash);
+  console.log("PD HASH", pd.hash);
   if (hash == pd.hash) {
-    sendResponse(res, 200, 'success', pd.status);
+    sendResponse(res, 200, "success", pd.status);
   } else {
-    sendResponse(res, 400, 'hash does not match');
+    sendResponse(res, 400, "hash does not match");
   }
 };

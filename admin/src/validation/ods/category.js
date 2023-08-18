10bfc8ef705dc80110be7cc597b3acda952170a8
“Iliyasooo7” <iliyas.shaikh19@vit.edu>
2023-08-18 15:17:46 +0530

    crud changes

--- admin/src/validation/ods/category.js
+++ admin/src/validation/ods/category.js
@@ -1,12 +1,16 @@
-import joi from 'joi';
-import validators from '../../../../shared/utils/validators.js';
+import joi from "joi";
+import validators from "../../../../shared/utils/validators.js";
 
 export const createCategory = joi.object({
   name: joi.string().required(),
-  image: validators.fileKey('category/images'),
+  icon: validators.fileKey("category/images"),
+  serviceCount: joi.number(),
+  orderCount: joi.number(),
 });
 
 export const updateCategory = joi.object({
   name: joi.string(),
-  image: validators.fileKey('category/images'),
+  image: validators.fileKey("category/images"),
+  serviceCount: joi.number(),
+  orderCount: joi.number(),
 });

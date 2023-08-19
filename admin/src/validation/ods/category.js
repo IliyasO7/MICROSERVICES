be291a77993eb090cacacdd8311fd82dd3d2dc15
Yuva <yuvaraj.poojary@bksgold.com>
2023-08-19 11:35:54 +0530

    feat: category

--- admin/src/validation/ods/category.js
+++ admin/src/validation/ods/category.js
@@ -1,16 +1,12 @@
-import joi from "joi";
-import validators from "../../../../shared/utils/validators.js";
+import joi from 'joi';
+import validators from '../../../../shared/utils/validators.js';
 
 export const createCategory = joi.object({
   name: joi.string().required(),
-  icon: validators.fileKey("category/images"),
-  serviceCount: joi.number(),
-  orderCount: joi.number(),
+  icon: validators.fileKey('category/images'),
 });
 
 export const updateCategory = joi.object({
   name: joi.string(),
-  image: validators.fileKey("category/images"),
-  serviceCount: joi.number(),
-  orderCount: joi.number(),
+  icon: validators.fileKey('category/images'),
 });
